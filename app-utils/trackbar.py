67b6483c20fa5ebdd563c2359342adca2b505aaf
han-nwin <hannguyen.win@gmail.com>
2025-02-22 10:55:25 -0600

    app utils

---
+++ app-utils/trackbar.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+from __future__ import division
+import cv2 as cv
+import argparse
+
+alpha_slider_max = 100
+title_window = "Linear Blend"
+
+
+def on_trackbar(val):
+    # NOTE: Alpha blend logic here
+    alpha = val / alpha_slider_max
+    beta = 1.0 - alpha
+    dst = cv.addWeighted(src1, alpha, src2, beta, 0.0)
+    cv.imshow(title_window, dst)
+
+
+parser = argparse.ArgumentParser(
+    description="Code for Adding a Trackbar to our applications tutorial."
+)
+parser.add_argument(
+    "--input1", help="Path to the first input image.",
+    default="../asset/LinuxLogo.jpg"
+)
+parser.add_argument(
+    "--input2", help="Path to the second input image.",
+    default="../asset/WindowsLogo.jpg"
+)
+args = parser.parse_args()
+
+# NOTE: First, we load two images, which are going to be blended
+src1 = cv.imread(cv.samples.findFile(args.input1))
+src2 = cv.imread(cv.samples.findFile(args.input2))
+
+if src1 is None:
+    print("Could not open or find the image: ", args.input1)
+    exit(0)
+
+if src2 is None:
+    print("Could not open or find the image: ", args.input2)
+    exit(0)
+
+# NOTE: To create a trackbar, first we have to create the window
+# in which it is going to be located
+cv.namedWindow(title_window)
+
+# Now we can create the Trackbar:
+trackbar_name = "Alpha x %d" % alpha_slider_max
+cv.createTrackbar(trackbar_name, title_window, 0,
+                  alpha_slider_max, on_trackbar)
+
+# Show some stuff
+# with the callback function
+on_trackbar(0)
+
+# Wait until user press some key
+cv.waitKey()
